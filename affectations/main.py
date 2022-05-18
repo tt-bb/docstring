855ef312eb144ddc4189d731166b140ac19c9b58
tt-bb <ttbb_@outlook.com>
2022-05-18 02:01:22 +0200

    Commit initial

---
+++ affectations/main.py
@@ -0,0 +1,15 @@
+a = 500
+print(f"a = {a}")
+langage = "python"
+print(f"Langage = {langage}")
+b = a
+print(f"b = {b}")
+c = d = 600
+print(f"c = {c}")
+print(f"d = {d}")
+e, f = 8, 12
+print(f"e = {e}")
+print(f"f = {f}")
+e, f = f, e
+print(f"e = {e}")
+print(f"f = {f}")
