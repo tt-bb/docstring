077006ecbd2593d95544dfeb0543d973ffae989f
tt-bb <ttbb_@outlook.com>
2022-05-19 12:38:22 +0200

    Commit initial

---
+++ parametres-defaut/main.py
@@ -0,0 +1,10 @@
+def compute(a=4, b=2, c=2, d=5, e=0):
+    # Ne modifiez pas la ligne ci-dessous
+    return str(a - b + c) * d
+
+
+# Ne modifiez pas la ligne ci-dessous
+resultat = compute()
+
+# TEST
+print(resultat)
